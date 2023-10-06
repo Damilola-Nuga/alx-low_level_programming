1f8a67557a8ee5769a6cdde649b64a3d136bd3aa
Damilola-Nuga <damiloladaniel357@gmail.com>
2023-10-05 18:02:21 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main put function
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is lke building a multilingual puzzle");
+	return (0);
+}
