d07b45c9e171e5496a7af4bc263e1781b7b13b87
Damilola-Nuga <damiloladaniel357@gmail.com>
2023-10-10 09:25:16 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+void print_alphabet_x10(void)
+
+{
+int i;
+
+for (i = 0; i <= 9; ++i)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
+}
