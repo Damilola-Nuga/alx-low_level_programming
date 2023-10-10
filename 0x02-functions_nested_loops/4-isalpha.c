6b4e6c8515ce97d44b06e0c9cdfd77fcec3b2527
Damilola-Nuga <damiloladaniel357@gmail.com>
2023-10-10 11:13:10 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if character is an alphabet
+ * @c - argument for the function
+ * Return: 1 - if letter, lowercase or uppercase, 0 - if otherwise
+ */
+
+int _isalpha(int c)
+{
+
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
