5a184c7ce6ce82f96a2a9580b6a31c02cb07e80f
Damilola-Nuga <damiloladaniel357@gmail.com>
2023-10-12 16:48:55 -0700

    0-main.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - this is the main function
+ * positive_or_negative - check the code
+ * @n: variable to be checked
  */
 
 void  positive_or_negative(int n)
